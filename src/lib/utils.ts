2db1bd2ceda2e4a223d2c129457efdaf8329bc2f
Elijah <elijahladdiedv@gmail.com>
2025-09-05 20:52:17 +0200

    move configs in env

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,14 +1,16 @@
 import { clsx, type ClassValue } from "clsx"
+import axios from "axios";
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-import axios from "axios";
+
+
 
 const api = axios.create({
-  baseURL: "http://localhost:8000", // Change to your FastAPI base URL
+  baseURL: import.meta.env.VITE_API_BASE_URL, // Read from environment variable
   headers: {
     "Content-Type": "application/json",
   },
